9ced765ea4b78d01b34825f1a0b76ab4ecaeb601
NycteaScandiaca <NycteaScandiaca@bk.ru>
2015-11-16 20:08:46 +0400

    :P

--- org.nycteascandiaca.prototypes.web/presentation/src/main/java/org/nycteascandiaca/prototypes/web/presentation/Application.java
+++ org.nycteascandiaca.prototypes.web/presentation/src/main/java/org/nycteascandiaca/prototypes/web/presentation/Application.java
@@ -1,16 +1,11 @@
 package org.nycteascandiaca.prototypes.web.presentation;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("org.nycteascandiaca.prototypes.web")
 public class Application extends SpringBootServletInitializer
 {
 	@Override
@@ -21,102 +16,6 @@ public class Application extends SpringBootServletInitializer
 	
 	public static void main(String[] args)
 	{
-		ApplicationContext context = SpringApplication.run(Application.class, args);
-		
-		//scanBeans(context);
-		
-		//createModels(context);
-		
-		//modifyModels(context);
-		
-		//scanAllModels(context);
+		SpringApplication.run(Application.class, args);
 	}
-	
-	/*private static void scanBeans(ApplicationContext context)
-	{
-		System.out.println("[Begin] Scan beans");
-		String[] beanNames = context.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames)
-        {
-            System.out.println(beanName);
-        }
-        System.out.println("[End] Scan beans");
-	}
-/*
-	private static void scanAllModels(ApplicationContext context)
-	{
-		ModelManager modelManager = (ModelManager)context.getBean("modelManager");
-		List<Model> allModels = modelManager.getAllModels();
-		for (Model current : allModels)
-		{
-			System.out.println(toString(current));
-		}
-	}
-	
-	private static void createModels(ApplicationContext context)
-	{
-		ModelRepository repository = context.getBean(ModelRepository.class);
-		for (int i = 1; i < 6; i++)
-		{
-			Model model = createModel(i);
-			repository.save(model);
-		}
-	}
-	
-	private static void modifyModels(ApplicationContext context)
-	{
-		System.out.println("[Begin] Modify Models");
-		ModelRepository repository = context.getBean(ModelRepository.class);
-		for (Model model : repository.findAll())
-		{
-			List<Parameter> parameters = model.getParameters();
-			parameters.remove(0);
-			repository.save(model);
-		}
-		System.out.println("[End] Modify Models");
-	}
-	
-	private static Model createModel(int index)
-	{
-		Model model = new Model();
-
-		model.setName("Model " + index);
-
-		List<Parameter> parameters = new ArrayList<Parameter>();
-		for (int i = 0; i < 5; i++)
-		{
-			Parameter parameter = new Parameter();
-			parameter.setName("Model" + index + ".Parameter");
-			parameter.setValue("Value." + Integer.toString(i));
-			parameters.add(parameter);
-		}
-
-		model.setParameters(parameters);
-		 
-
-		return model;
-	}
-
-	private static String toString(Model model)
-	{
-		StringBuffer sb = new StringBuffer();
-
-		sb.append("Model {\n");
-		sb.append("\tid: ").append(model.getId()).append("\n");
-		sb.append("\tname: ").append(model.getName()).append("\n");
-
-		sb.append("\tparameters {\n");
-
-		for (Parameter parameter : model.getParameters())
-		{
-			sb.append("\t\t{name: ").append(parameter.getName());
-			sb.append(", value: ").append(parameter.getValue());
-			sb.append("}\n");
-		}
-		 
-		sb.append("\t}\n}");
-
-		return sb.toString();
-	}*/
 }
