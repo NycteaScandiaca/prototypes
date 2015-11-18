9d8d6999cccaf3671252cd1003f766da7e4929a2
NycteaScandiaca <NycteaScandiaca@bk.ru>
2015-11-18 16:10:57 +0400

    Organize import

--- org.nycteascandiaca.prototypes.web/presentation/src/main/java/org/nycteascandiaca/prototypes/web/presentation/client/parameters/ParametersView.java
+++ org.nycteascandiaca.prototypes.web/presentation/src/main/java/org/nycteascandiaca/prototypes/web/presentation/client/parameters/ParametersView.java
@@ -1,13 +1,7 @@
 package org.nycteascandiaca.prototypes.web.presentation.client.parameters;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.nycteascandiaca.prototypes.web.domain.Parameter;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
